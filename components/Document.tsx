f7771f35bc0d30cff602711593d1c557d57aa4cf
Tejas-Naik <rntejas2005@gmail.com>
2024-07-25 00:56:39 +0530

    Completed Day2 with functionality to add the documents

---
+++ components/Document.tsx
@@ -0,0 +1,13 @@
+import PlaceHolderDocument from "./PlaceHolderDocument"
+
+function Documents() {
+  return (
+    <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
+        {/* <ap through the document */}
+
+        <PlaceHolderDocument/>
+    </div>
+  )
+}
+
+export default Documents 
