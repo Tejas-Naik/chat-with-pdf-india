4ae8dd6cf82f9325d32ad3933c61c1592ad4a7ec
Tejas-Naik <rntejas2005@gmail.com>
2024-10-31 13:01:04 +0530

    Pricing Page start

---
+++ app/dashboard/upgrade/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type Props = {};
+
+function PricingPage({}: Props) {
+  return (
+    <div>
+      <div className="py-24 sm:py-32">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+            Pricing
+          </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Supercharge your Document companion
+          </p>
+        </div>
+        <p
+          className="mx-auto mt-6 max-w-2xl px-10 text-center text-lg leading-8 text-gray-600"
+        >
+          Choose an affordable plan thats packed with the best features for
+          interacting with your PDFs, enhancing productivity, and streamlining
+          your workflow.
+        </p>
+        
+        I
+      </div>
+    </div>
+  );
+}
+
+export default PricingPage;
